7773d01c9c15df7d72aeb73d868871b9bb993c6d
Lewis Myllylä <yllillilly9@gmail.com>
2024-10-01 15:56:38 +0200

    HONSE

---
+++ Assets/Scripts/DrawWithMouse.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawWithMouse : MonoBehaviour
+{
+    private LineRenderer line;
+    private Vector3 previousPosition;
+
+    [SerializeField]
+    private float minDistance = 0.1f;
+
+    private void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.positionCount = 1;
+        previousPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 temp = Input.mousePosition;
+            temp.z = -Camera.main.transform.position.z;
+            Vector3 currentPositon = Camera.main.ScreenToWorldPoint(temp);
+            currentPositon.z = 0f;
+            
+            if (Vector3.Distance(currentPositon, previousPosition) > minDistance)
+            {
+                if (previousPosition == transform.position)
+                {
+                    line.SetPosition(0, currentPositon);
+                }
+                else
+                {
+                    line.positionCount++;
+                    line.SetPosition(line.positionCount - 1, currentPositon);
+                }
+                
+                previousPosition = currentPositon;
+            }
+        }
+    }
+}
